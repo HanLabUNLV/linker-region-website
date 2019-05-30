744e8549a9f8e2f897582f5fc639fbd34432f7ff
adrian-alberto <adriansalberto@gmail.com>
2019-05-30 12:26:45 -0700

    id get test

--- query.php
+++ query.php
@@ -1,14 +1,22 @@
+<?php
+
+$id = '?';
+if (isset*$_GET['id']) {
+	$id = $_GET['id'];
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>test</title>
+	<title><?php echo $id; ?> : Linker Region Query Results</title>
 	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:400,700&display=swap" rel="stylesheet"> 
 	<link href="/linkerregions/main.css" rel="stylesheet">
 </head>
 
 
 <body>
-	test page
+	test page<br>
 
 	<?php
 	echo "Hello World!";
