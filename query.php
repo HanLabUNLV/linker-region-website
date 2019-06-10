0f872f6c672a61acf263ea0739d0b6ee7dcae50b
adrian-alberto <adriansalberto@gmail.com>
2019-06-10 22:22:13 +0000

    display regions, coords

--- query.php
+++ query.php
@@ -36,15 +36,45 @@ if (isset($_GET['id']))
 	<hr>
 
 	<?php
-	$fh = fopen('data/stratified.txt', 'r');
+	$fh = fopen('data/superfamily_sorted_shifted_dict_stratified.json', 'r');
 	$n = 0;
 	while ($line = fgets($fh)) {
 		$tab = strpos($line, "\t");
 		$line_id = substr($line, 0, $tab);	
-		
+		$line_json = substr($line, $tab+1);
 		if (substr($line_id, 0, strlen($id)) == $id) {
+
+			//load appropriate fasta file
+			$fasta_h = fopen('data/fastas/' . $line_id . '.fasta.fas', 'r');
+
+			$sequences = [];
+			$nextSeq = NULL;
+			while ($x = fgets($fasta_h)) {
+				if (substr($x, 0, 1) == ">") {
+					$nextSeq = trim(substr($x,1));
+					$sequences[$nextSeq] = "";
+					
+				} else {
+					$sequences[$nextSeq] = $sequences[$nextSeq] . trim($x);
+				}
+			}
+			fclose($fasta_h);
+			
+
+			$json_obj = json_decode($line_json);
+			
+			foreach ($json_obj as $protein_id => $idk) {
+				echo $protein_id . "<br>";
+				
+				foreach($idk as $tup)
+					echo $tup[0] . "," . $tup[1];
+
+	
+				echo '<div class="region-figure">' . $sequences[$protein_id] . '</div><br>';
+			}	
+
 			echo "<h3 id='" . $line_id . "'>" . $line_id . "</h3>";
-			echo "<code>" . $line . "</code><br>";	
+			echo "<code>" . $line_json . "</code><br>";	
 		}	
 	}
 	fclose($fh)
