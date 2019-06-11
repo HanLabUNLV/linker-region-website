e38c74829d6a95dbbe0222acd803611a92665f69
adrian-alberto <adriansalberto@gmail.com>
2019-06-11 15:32:57 -0700

    idlist page

---
+++ idlist.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gene Tree IDs</title>
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:400,700&display=swap" rel="stylesheet"> 
+	<link href="/linkerregions/main.css" rel="stylesheet">
+</head>
+
+
+<body>
+	<h1>Gene Tree IDs</h1>
+	<ul>
+		<?php
+		$fh = fopen("/linkerregions/data/idlist.txt", "r");
+		while ($line = fgets($fh)) {
+			$id = trim($line);
+			echo '<li><a href="/linkerregions/query.php?id="' . $id . '">' . $id . '</a></li>';
+		}
+
+
+		?>
+
+
+
+	</ul>
+
+
+</body>
