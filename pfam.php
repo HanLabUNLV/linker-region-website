ab4010744d6de7329dd08f429ff8c75d3c2095df
adrian-alberto <adriansalberto@gmail.com>
2019-09-30 14:08:33 -0700

    new pfam page

---
+++ pfam.php
@@ -0,0 +1,23 @@
+<?php
+
+$id = '?';
+if (isset($_GET['id']))
+	$id = trim($_GET['id']);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $id; ?> : Linker Region Query Results</title>
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:400,700&display=swap" rel="stylesheet"> 
+	<link href="/linkerregions/main.css" rel="stylesheet">
+</head>
+
+
+<body>
+	<?php
+		echo $id
+	?>
+
+
+</body>
