088c706a0182124c7a97921866751b950b85b1d6
adrian-alberto <adriansalberto@gmail.com>
2019-10-01 15:40:56 -0700

    Add domain page

---
+++ domain.php
@@ -0,0 +1,48 @@
+<?php
+
+$id = '?';
+if (isset($_GET['id']))
+	$id = trim($_GET['id']);
+
+$query = "https://hanlab.pythonanywhere.com/linkerregions/domainid?domain=$id";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $query);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+$output = curl_exec($ch);
+curl_close($ch);
+
+$domain_sequences = json_decode($output);
+$gt = substr($id, 0,strpos($id, "Domain")-1);
+?>
+
+<!DOCTYPE=html>
+<head>
+        <title><?php echo $id; ?> : Domain Region Family</title>
+        <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:400,700&display=swap" rel="stylesheet">
+        <link href="/linkerregions/main.css" rel="stylesheet">
+</head>
+<body>
+
+	<a class="fancy-link" href="/linkerregions">Home</a><br>
+
+	<h1><?php echo $id?></h1>
+<?php
+	echo "<p>(From gene tree: <a class='fancy-link' href='/linkerregions/query?id=$gt'>$gt</a>)</p>";
+
+	echo "<p><b>Query using REST API:</b></p>";
+	echo "<code>$query</code>";
+	echo "<hr>";
+
+	foreach ($domain_sequences as $domainId => $seq) {
+		echo "<h2>>$domainId</h2>";
+		echo "<code>$seq</code>";
+	}
+
+?>
+
+
+</body>
+</html>
+
