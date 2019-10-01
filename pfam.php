21b148ba04dbac8dfe12b9c6bb9da80fe43c3495
adrian-alberto <adriansalberto@gmail.com>
2019-10-01 14:10:51 -0700

    Change pfam page title

--- pfam.php
+++ pfam.php
@@ -22,7 +22,7 @@ $linker_neighbors = $pfam_data->linker_neighbors;
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $id; ?> : Linker Region Query Results</title>
+	<title><?php echo $id; ?> : Pfam Query Results</title>
 	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:400,700&display=swap" rel="stylesheet"> 
 	<link href="/linkerregions/main.css" rel="stylesheet">
 </head>
