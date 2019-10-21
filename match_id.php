6d95f34e7e9bbacf5ef515ef2a357750e533f9d1
adrian-alberto <adriansalberto@gmail.com>
2019-10-21 12:37:53 -0700

    small fix

--- match_id.php
+++ match_id.php
@@ -5,13 +5,13 @@ if (isset($_GET['id']))
 	$id = trim($_GET['id']);
 
 if (preg_match('/^ENSGT[0-9]+$/', $id) == 1)
-	printf("Matches gene tree")
+	printf("Matches gene tree");
 
 if (preg_match('/^ENS[A-Z]*P[0-9]+$/', $id) == 1)
-	printf("Matches protein")
+	printf("Matches protein");
 
 if (preg_match('/^PF[0-9]+$/', $id) == 1)
-	printf("Matches pfam")
+	printf("Matches pfam");
 
 
 
