9be9870777a00ed1ab5fd59a88fec55911475e21
adrian-alberto <adriansalberto@gmail.com>
2019-10-21 13:22:41 -0700

    add pfam id search support

--- match_id.php
+++ match_id.php
@@ -22,6 +22,8 @@ if (preg_match('/^PF[0-9]+$/', $id) == 1)
 		var inp = document.getElementById("idSearchBox").value;
 		if (inp.match(/^ENSGT[0-9_]+$/))
 			location.href = "/linkerregions/query.php?id="+inp;
+		else if (inp.match(/^PF[0-9]+$/))
+			location.href = "/linkerregions/pfam.php?id="+inp;
 		else
 			alert("Invalid id");
 	}
