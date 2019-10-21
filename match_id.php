c43224c9790c718621525aadaf514ddd7d5e7991
adrian-alberto <adriansalberto@gmail.com>
2019-10-21 14:10:15 -0700

    Added linker + domain search support

--- match_id.php
+++ match_id.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/* TODO: add these regex checks to python server?
 $id = '?';
 if (isset($_GET['id']))
 	$id = trim($_GET['id']);
@@ -13,23 +13,26 @@ if (preg_match('/^ENS[A-Z]*P[0-9]+$/', $id) == 1)
 if (preg_match('/^PF[0-9]+$/', $id) == 1)
 	printf("Matches pfam");
 */
-
-
-
 ?>
 <script>
 	function redirect() {
 		var inp = document.getElementById("idSearchBox").value;
 		if (inp.match(/^ENSGT[0-9_]+$/))
 			location.href = "/linkerregions/query.php?id="+inp;
+		else if (inp.match(/^ENSGT[0-9_]+_Domain_[0-9]+$/))
+			location.href = "/linkerregions/domain.php?id="+inp;
+		else if (inp.match(/^ENSGT[0-9_]+_Linker_[0-9]+$/))
+			location.href = "/linkerregions/linker.php?id="+inp;
 		else if (inp.match(/^PF[0-9]+$/))
 			location.href = "/linkerregions/pfam.php?id="+inp;
+		else if (inp.match(/^ENS[A-Z]*P[0-9]+$/))
+			alert("Protein ID not yet supported")
 		else
 			alert("Invalid id");
 	}
-
 </script>
+
 <form action="javascript:redirect();">
-	<input id="idSearchBox" type="text" name="id"/>
-	<input type="submit" value="Search"/>
+	<input id="idSearchBox" type="text" name="id">
+	<input type="submit" value="Search">
 </form>
