7674d0d3749e70408f74db6c6ea7beb63806a298
adrian-alberto <adriansalberto@gmail.com>
2019-10-21 13:08:05 -0700

    try sending id using js

--- match_id.php
+++ match_id.php
@@ -21,11 +21,11 @@ if (preg_match('/^PF[0-9]+$/', $id) == 1)
 	function redirect() {
 		var inp = document.getElementById("idSearchBox").value;
 		alert("!!!!");
-		location.href = "/linkerregions/query.php";
+		location.href = "/linkerregions/query.php?id="+inp;
 	}
 
 </script>
-<form action="javascript:redirect();" method="get">
+<form action="javascript:redirect();">
 	<input id="idSearchBox" type="text" name="id"/>
 	<input type="submit" value="Search"/>
 </form>
