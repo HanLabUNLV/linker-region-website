eb7ac33ca695a04273ff8f96050b63ae8c29bdc3
adrian-alberto <adriansalberto@gmail.com>
2019-10-21 13:04:18 -0700

    searchbox redirect js test

--- match_id.php
+++ match_id.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $id = '?';
 if (isset($_GET['id']))
 	$id = trim($_GET['id']);
@@ -12,10 +12,20 @@ if (preg_match('/^ENS[A-Z]*P[0-9]+$/', $id) == 1)
 
 if (preg_match('/^PF[0-9]+$/', $id) == 1)
 	printf("Matches pfam");
-
+*/
 
 
 
 ?>
+<script>
+	function redirect() {
+		var inp = document.getElementById("idSearchBox").value;
+		alert("!!!!");
+		location.href = "/linkerregions/query.php";
+	}
 
-<h2> hello world</h2>
+</script>
+<form action="javascript:redirect();">
+	<input id="idSearchBox" type="text" name="id"/>
+	<input type="submit" value="Search"/>
+</form>
