c54483cdff5f93d0ce284b1398580a28475b3c59
adrian-alberto <adriansalberto@gmail.com>
2019-10-30 15:20:08 -0700

    Update index to use universal search bar

--- index.php
+++ index.php
@@ -11,35 +11,26 @@
 
 
 <p>In most genes there are several functional domains and non-functional sequences of DNA in between them. Linker regions are the segments of DNA between functional domains that act as a bridge between the different regions of proteins. These regions have been known to have a higher mutation rate than is present in their functional domain counterparts, suggesting that the composition of these regions may be less important than that of other regions and also a unique evolutionary history. Whether it is aiding in the structural analysis of proteins or adding to the base knowledge of protein engineering, the study of linker regions is a rich if understudied subject. This database will not only allow our own research on linker regions to be conducted, but also is made public so that other researchers may study the evolution of linker regions.</p>
+  <div style="display:flex;">
+    <div style="width:50%;">
+    <h2>Enter a new query:</h2>
+    <?php
+      include 'match_id.php';
+    ?>
+    </div>
+    <div style="width:50%;">
+    <h2>Example searches:</h2>
+    <p>
+      Gene Tree: <span>ENSGT00680000099553_8</span><br>
+      Gene ID: <span>ENSG00000007372</span><br>
+      PFAM ID: <span>PF00096</span> (This one may take a while to load)<br>
 
-<h2>Enter a new query:</h2>
+      Linker ID: <span>ENSGT00680000099553_8_Linker_0_1</span><br>
+      Domain ID: <span>ENSGT00680000099553_8_Domain_0</span><br>
 
-<form action="/linkerregions/query.php">
-  <p>Query by Gene Tree ID:</p>
-  <input type="text" name="id">
-  <input type="submit" value="Submit">
-</form>
-
-<form action="/linkerregions/pfam.php">
-  <p>Query by PFAM ID:</p>
-  <input type="text" name="id">
-  <input type="submit" value="Submit">
-</form>
-
-
-<form action="/linkerregions/linker.php">
-  <p>Query by Linker ID:</p>
-  <input type="text" name="id">
-  <input type="submit" value="Submit">
-</form>
-
-
-<form action="/linkerregions/domain.php">
-  <p>Query by Domain ID:</p>
-  <input type="text" name="id">
-  <input type="submit" value="Submit">
-</form>
-
+    </p>
+    </div>
+  </div>
 
 <p>
   You can view the full list of Gene Tree IDs we have here:<br>
